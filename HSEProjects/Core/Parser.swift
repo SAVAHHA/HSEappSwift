4f01ff56ef23a90832ca142c916234a006505035
Pavel <petrov.pavel.v14@gmail.com>
2021-02-07 18:35:47 +0300

    Project parser works in beta-mode

--- HSEProjects/Core/Parser.swift
+++ HSEProjects/Core/Parser.swift
@@ -8,18 +8,43 @@
 
 import Foundation
 import SwiftSoup
+import Kanna
 
 class Parser{
     var projectList = [Project]()
     func parseProjects() -> String{
         let url = URL(string: "https://pf.hse.ru/")
         var html: String
+        var projectName: String = ""
+        var projectItem: String = ""
         do{
             try html = String(contentsOf: url!)
         }
         catch{
             html = "error"
         }
-        return html
+        if let doc = try? HTML(html: html, encoding: .utf8) {
+            // Search for nodes by XPath
+
+            for item in doc.xpath("//div[@class = 'posts_group_clean']/div"){
+                let projectString = item
+                projectName = item.at_xpath("//h3[@class = 'first_child']")!.text ?? "none"
+                projectItem = ""
+                var n = 1
+                for item in projectString.xpath("//div[@class = 'small with-indent']"){
+                    let projectList = item
+                    for item in projectList.xpath("//div/span | //div/a"){
+                        projectItem += "\(String(describing: item.text!)) "
+                        if (n % 2 == 0){
+                            projectItem += "\n"
+                        }
+                        n += 1
+                    }
+                }
+                break
+            }
+        }
+        print(String((projectName ?? "none") + "\n" + projectItem))
+        return String((projectName ?? "none") + "\n" + projectItem)
     }
 }
